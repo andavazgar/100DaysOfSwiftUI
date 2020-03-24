3e7999bf5b369d32f656de81ea56084fbe814016
Andrés Vazquez <andavazgar.dev@gmail.com>
2020-03-24 12:57:04 -0400

    (Days 17, 18) Project 1
    - Removed the project overview code
    - Completed the project + challenges

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -9,55 +9,67 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = "2"
+    @State private var tipPercentage = 2
+    
+    private let tipPercentages = [5, 10, 15, 20, 25]
+    private var total: Double {
+        let orderAmount = Double(checkAmount) ?? 0
+        let tipSelection = Double(tipPercentages[tipPercentage]) / 100
+        
+        return orderAmount * (1 + tipSelection)
+    }
+    private var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople) ?? 1
+        
+        return total / peopleCount
+    }
+    
     var body: some View {
         NavigationView {
             Form {
                 Section {
-                    Text("Hello, World!")
-                    Text("Hello, World!")
-                    Text("Hello, World!")
+                    HStack {
+                        Text("Amount")
+                        TextField("", text: $checkAmount)
+                            .multilineTextAlignment(.trailing)
+                            .keyboardType(.decimalPad)
+                    }
+                    
+                    HStack {
+                        Text("Number of people")
+                        TextField("", text: $numberOfPeople)
+                            .multilineTextAlignment(.trailing)
+                            .keyboardType(.numberPad)
+                    }
                 }
                 
-                Section {
-                    Text("Hello, World!")
-                    Text("Hello, World!")
-                    Text("Hello, World!")
+                Section(header: Text("How much tip do you want to leave?")) {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< self.tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
                 }
                 
-                NavigationLink("Go to next screen", destination: ContentViewWithState())
+                Section(header: Text("Total")) {
+                    Text("$\(total, specifier: "%.2f")")
+                }
+                
+                Section(header: Text("Amount per person")) {
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                }
             }
-            .navigationBarTitle("SwiftUI")
+            .navigationBarTitle("WeSplit")
         }
     }
 }
 
-struct ContentViewWithState: View {
-    @State private var tapCount = 0
-    let students = ["Harry", "Herminone", "Ron"]
-    @State private var selectedStudent = 0
-    
-    var body: some View {
-        VStack {
-            Button("Tap Count: \(tapCount)") {
-                self.tapCount += 1
-            }
-            Picker("Select your student", selection: $selectedStudent) {
-                ForEach(0 ..< students.count) {
-                    Text(self.students[$0])
-                }
-            }
-            Text("You chose: \(students[selectedStudent])")
-        }
-    }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        Group {
-            ContentView()
-                .previewDisplayName("ContentView")
-            ContentViewWithState()
-            .previewDisplayName("ContentViewWithState")
-        }
+        ContentView()
     }
 }

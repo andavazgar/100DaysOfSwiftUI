395ebf44963967bb73b310b1a0f995aa52ad3333
Andrés Vazquez <andavazgar.dev@gmail.com>
2020-04-27 14:46:40 -0400

    (Day 39) Project 8: Moonshot
    Practice with new techniques: GeometryReader, ScrollView, NavigationLink and Decoding nested objects.

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -10,13 +10,60 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            GeometryReader { geo in
-                Image("hws")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: geo.size.width)
+        NavigationView {
+            VStack {
+                GeometryReader { geo in
+                    Image("hws")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: geo.size.width)
+                }
+    
+                ScrollView {
+                    VStack(spacing: 10) {
+                        ForEach(0..<100) {
+                            Text("Item \($0)")
+                                .font(.title)
+                        }
+                    }
+                    .frame(maxWidth: .infinity)
+                }
+                
+                NavigationLink(destination: Text("Details View")) {
+                    Text("Hello World")
+                }
+                .padding(.vertical, 25)
+                
+                Button("Decode JSON") {
+                    let input = """
+                    {
+                        "name": "Taylor Swift",
+                        "address": {
+                            "street": "555, Taylor Swift Avenue",
+                            "city": "Nashville"
+                        }
+                    }
+                    """
+                    
+                    struct User: Codable {
+                        var name: String
+                        var address: Address
+                    }
+                    
+                    struct Address: Codable {
+                        var street: String
+                        var city: String
+                    }
+
+                    let data = Data(input.utf8)
+                    let decoder = JSONDecoder()
+                    
+                    if let user = try? decoder.decode(User.self, from: data) {
+                        print(user.address.street)
+                    }
+                }
             }
+            .navigationBarTitle("SwiftUI")
         }
     }
 }

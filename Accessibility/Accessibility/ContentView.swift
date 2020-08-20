355660bdef19d25b78c94c6abb8cb554b5d5b445
Andrés Vazquez <andavazgar.dev@gmail.com>
2020-08-19 23:59:07 -0400

    (Day 74) Project 15: Accessibility - Accessibility label, value, traits and elements / groups

---
+++ Accessibility/Accessibility/ContentView.swift
@@ -0,0 +1,59 @@
+//
+//  ContentView.swift
+//  Accessibility
+//
+//  Created by Andres Vazquez on 2020-08-19.
+//  Copyright © 2020 Andavazgar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let pictures = [
+        "ales-krivec-15949",
+        "galina-n-189483",
+        "kevin-horstmann-141705",
+        "nicolas-tissot-335096"
+    ]
+    
+    let labels = [
+        "Tulips",
+        "Frozen tree buds",
+        "Sunflowers",
+        "Fireworks"
+    ]
+    
+    @State private var selectedPicture = Int.random(in: 0...3)
+    @State private var rating = 3
+    
+    var body: some View {
+        VStack {
+            Image(pictures[selectedPicture])
+                .resizable()
+                .scaledToFit()
+                .accessibility(label: Text(labels[selectedPicture]))
+                .accessibility(addTraits: [.isButton])
+                .accessibility(removeTraits: [.isImage])
+                .onTapGesture {
+                    self.selectedPicture = Int.random(in: 0...3)
+            }
+            
+            VStack {
+                Text("Your score is")
+                Text("1000")
+                    .font(.title)
+            }
+            .accessibilityElement(children: .ignore)
+            .accessibility(label: Text("Your score is 1000"))
+            
+            Stepper("Rate our service: \(rating)/5", value: $rating, in: 1...5)
+                .accessibility(value: Text("\(rating) out of 5"))
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

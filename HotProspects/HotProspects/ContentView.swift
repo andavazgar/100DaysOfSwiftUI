42d375fb25291d0f2a5f201e6410c44a49268d5a
Andrés Vazquez <andavazgar.dev@gmail.com>
2020-08-30 23:59:21 -0400

    (Day 81) Project 16: HotProspects - Added a context menu - Added a local notification

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -9,12 +9,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var backgroundColor = Color.red
+    
     var body: some View {
         TabView {
             Text("Tab 1")
+                .background(backgroundColor)
                 .tabItem {
                     Image(systemName: "star")
                     Text("Tab 1")
+                }
+                .contextMenu {
+                    Button(action: {
+                        self.backgroundColor = .red
+                    }) {
+                        Text("Red")
+                        Image(systemName: "1.circle")
+                    }
+
+                    Button(action: {
+                        self.backgroundColor = .green
+                    }) {
+                        Text("Green")
+                        Image(systemName: "2.circle")
+                    }
+
+                    Button(action: {
+                        self.backgroundColor = .blue
+                    }) {
+                        Text("Blue")
+                        Image(systemName: "3.circle")
+                    }
+                }
+                .onTapGesture {
+                    UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                        if success {
+                            let content = UNMutableNotificationContent()
+                            content.title = "Feed the cat"
+                            content.subtitle = "It looks hungry"
+                            content.sound = UNNotificationSound.default
+                            
+                            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+
+                            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+
+                            UNUserNotificationCenter.current().add(request)
+                        } else if let error = error {
+                            print(error.localizedDescription)
+                        }
+                    }
                 }
             
             Text("Tab 2")

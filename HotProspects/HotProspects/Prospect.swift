8d1c614fb1e38731cfe46fb057b1f686c8c861aa
Andrés Vazquez <andavazgar.dev@gmail.com>
2020-09-02 00:43:51 -0400

    (Day 84) Project 16: HotProspects - Added data persistence with UserDefaults - Added local notification feature

--- HotProspects/HotProspects/Prospect.swift
+++ HotProspects/HotProspects/Prospect.swift
@@ -16,14 +16,34 @@ class Prospect: Identifiable, Codable {
 }
 
 class Prospects: ObservableObject {
-    @Published var people: [Prospect]
+    @Published private(set) var people: [Prospect]
+    private static let saveKey = "SavedData"
     
     init() {
+        if let data = UserDefaults.standard.data(forKey: Self.saveKey) {
+            if let decoded = try? JSONDecoder().decode([Prospect].self, from: data) {
+                self.people = decoded
+                return
+            }
+        }
+        
         self.people = []
+    }
+    
+    func add(_ person: Prospect) {
+        self.people.append(person)
+        save()
+    }
+    
+    private func save() {
+        guard let encoded = try? JSONEncoder().encode(people) else { return }
+        
+        UserDefaults.standard.set(encoded, forKey: Self.saveKey)
     }
     
     func toggle(_ prospect: Prospect) {
         objectWillChange.send()
         prospect.isContacted.toggle()
+        save()
     }
 }
--- HotProspects/HotProspects/ProspectsView.swift
+++ HotProspects/HotProspects/ProspectsView.swift
@@ -8,6 +8,7 @@
 
 import CodeScanner
 import SwiftUI
+import UserNotifications
 
 struct ProspectsView: View {
     enum FilterType {
@@ -54,6 +55,12 @@ struct ProspectsView: View {
                         Button(prospect.isContacted ? "Mark Uncontacted" : "Mark Contacted" ) {
                             self.prospects.toggle(prospect)
                         }
+                        
+                        if !prospect.isContacted {
+                            Button("Remind Me") {
+                                self.addNotification(for: prospect)
+                            }
+                        }
                     }
                 }
             }
@@ -78,13 +85,47 @@ struct ProspectsView: View {
             let details = code.components(separatedBy: "\n")
             guard details.count == 2 else { return }
             
-            let prospect = Prospect()
-            prospect.name = details[0]
-            prospect.email = details[1]
-            self.prospects.people.append(prospect)
+            let person = Prospect()
+            person.name = details[0]
+            person.email = details[1]
+            self.prospects.add(person)
             
         case .failure(_):
             print("Scanning failed")
+        }
+    }
+    
+    func addNotification(for prospect: Prospect) {
+        let center = UNUserNotificationCenter.current()
+        
+        let addRequest = {
+            let content = UNMutableNotificationContent()
+            content.title = "Contact \(prospect.name)"
+            content.subtitle = prospect.email
+            content.sound = UNNotificationSound.default
+            
+//            var date = DateComponents()
+//            date.hour = 9
+//            let trigger = UNCalendarNotificationTrigger(dateMatching: date, repeats: false)
+            
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+            
+            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+            center.add(request)
+        }
+        
+        center.getNotificationSettings { (settings) in
+            if settings.authorizationStatus == .authorized {
+                addRequest()
+            } else {
+                center.requestAuthorization(options: [.alert, .badge, .sound]) { (success, error) in
+                    if success {
+                        addRequest()
+                    } else {
+                        print("Can't send notifications")
+                    }
+                }
+            }
         }
     }
 }

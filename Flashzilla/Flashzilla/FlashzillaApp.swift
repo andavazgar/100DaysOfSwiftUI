4f22062bd213d7d8302e6d0f12099cddff1fe14d
Andrés Vazquez <andavazgar.dev@gmail.com>
2020-09-30 17:24:28 -0400

    (Day 86) Project 17: Flashzilla - Created the project

---
+++ Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Andres Vazquez on 2020-09-30.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

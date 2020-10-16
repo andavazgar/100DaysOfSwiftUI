0938c7ddd8718335895000444c9b98e371536ed2
Andrés Vazquez <andavazgar.dev@gmail.com>
2020-10-15 21:26:27 -0400

    (Day 86) Project 17: Flashzilla

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -5,12 +5,116 @@
 //  Created by Andres Vazquez on 2020-09-30.
 //
 
+import CoreHaptics
 import SwiftUI
 
 struct ContentView: View {
+    @State private var currentAmount: CGFloat = 0
+    @State private var finalAmount: CGFloat = 1
+    
+    @State private var offset = CGSize.zero
+    @State private var isDragging = false
+    
+    @State private var engine: CHHapticEngine?
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        let dragGesture = DragGesture()
+            .onChanged { value in
+                offset = value.translation
+            }
+            .onEnded { _ in
+                withAnimation {
+                    offset = .zero
+                    isDragging = false
+                }
+            }
+        
+        let pressGesture = LongPressGesture()
+            .onEnded { _ in
+                withAnimation {
+                    isDragging = true
+                }
+            }
+        
+        let combinedGesture = pressGesture.sequenced(before: dragGesture)
+        
+        return VStack {
+            Text("Pinch me in or out. I can MAGNIFY!")
+                .scaleEffect(finalAmount + currentAmount)
+                .gesture(
+                    MagnificationGesture()
+                        .onChanged({ amount in
+                            currentAmount = amount - 1
+                        })
+                        .onEnded({ _ in
+                            finalAmount += currentAmount
+                            currentAmount = 0
+                        })
+            )
+            
+            Circle()
+                .fill(Color.red)
+                .frame(width: 64, height: 64)
+                .scaleEffect(isDragging ? 1.5 : 1)
+                .offset(offset)
+                .gesture(combinedGesture)
+            
+            Button("Haptic feedback", action: simpleHaptics)
+                .padding()
+            
+            Button("Custom haptic") {
+                complexHaptics()
+            }
+            .onAppear(perform: prepareHaptics)
+        }
+        .contentShape(Rectangle())
+        .onTapGesture() {
+            print("VStack tapped!")
+        }
+    }
+    
+    func simpleHaptics() {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(.success)
+    }
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        do {
+            self.engine = try CHHapticEngine()
+            try self.engine?.start()
+        } catch {
+            print("There was an error creating the engine: \(error.localizedDescription)")
+        }
+    }
+    
+    func complexHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else { return }
+        
+        var events = [CHHapticEvent]()
+        
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: i)
+            events.append(event)
+        }
+        
+        for i in stride(from: 0, to: 1, by: 0.1) {
+            let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: Float(1 - i))
+            let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: Float(1 - i))
+            let event = CHHapticEvent(eventType: .hapticTransient, parameters: [intensity, sharpness], relativeTime: 1 + i)
+            events.append(event)
+        }
+        
+        do {
+            let pattern = try CHHapticPattern(events: events, parameters: [])
+            let player = try engine?.makePlayer(with: pattern)
+            try player?.start(atTime: 0)
+        } catch {
+            print("Failed to play pattern: \(error.localizedDescription).")
+        }
     }
 }
 
